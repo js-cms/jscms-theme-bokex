6780ce86490ba5bb3527c9146606d157ad38434c
lisniuse <17560235@qq.com>
2019-05-13 18:36:13 +0800

    feat: improve.

--- dev.js
+++ dev.js
@@ -6,12 +6,12 @@ let themeConfigContent = fs.readFileSync(JscmsServerThemeConfig, 'utf-8');
 let reg = /^const\sTHEME_NAME\s=\s'(\w+)';$/gm;
 let constStr = reg.exec(themeConfigContent)[0];
 let newThemeConfigContent = themeConfigContent.replace(constStr, `const THEME_NAME = 'bokex';`);
-fs.writeFileSync(JscmsServerThemeConfig, newThemeConfigContent);
 
-console.log('\n');
-console.log(` write file ${JscmsServerThemeConfig}`);
+fs.writeFileSync(JscmsServerThemeConfig, newThemeConfigContent);
+// console.log(` write file ${JscmsServerThemeConfig}`);
 fs.removeSync(themeDir);
-console.log(` remove ${themeDir}`);
+// console.log(` remove ${themeDir}`);
 fs.copySync(config.themeDir, themeDir);
-console.log(` copy ${config.themeDir} to ${themeDir}`);
-console.log('\n');
+// console.log(` copy ${config.themeDir} to ${themeDir}`);
+// console.log('\n');
+console.log(` It's written.`);
