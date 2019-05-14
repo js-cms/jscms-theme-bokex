b2e762c60877785856e4e50f6afd604a39f467fa
lisniuse <17560235@qq.com>
2019-05-14 23:59:29 +0800

    feat: update style.

--- theme/bokex/static/js/common.js
+++ theme/bokex/static/js/common.js
@@ -1,5 +1,5 @@
 (function ($) {
-  //处理移动端导航
+  // 处理移动端导航
   $(document).ready(function () {
     "use strict";
     $('#primary-menu').slicknav({
@@ -9,16 +9,34 @@
     });
   });
 
-  //让右边栏固定
+  // 让右边栏固定
   $(document).ready(function () {
     "use strict";
     $(window).load(function () {
-      var stickySidebar = new StickySidebar('#secondary', {
-        topSpacing: 20,
-        bottomSpacing: 20,
-        containerSelector: '.site_container',
-        innerWrapperSelector: '.sidebar__inner'
-      });
+      if (document.querySelector('#secondary')) {
+        var stickySidebar = new StickySidebar('#secondary', {
+          topSpacing: 20,
+          bottomSpacing: 20,
+          containerSelector: '.site_container',
+          innerWrapperSelector: '.sidebar__inner'
+        });
+      }
+    });
+  });
+
+  // 点赞事件
+  $(".like-btn").on("click", function (e) {
+    if ($(this).hasClass('liked')) return;
+    var that = this;
+    $.post("/api/front/article/like", {
+      numberId: this.dataset.numberId
+    }, function (res) {
+      if (res.code === 0) {
+        $(that).addClass('liked');
+        $(that).find("#likeTotal").text(res.data.count);
+      } else {
+        alert(res.msg);
+      }
     });
   });
 })(jQuery);
